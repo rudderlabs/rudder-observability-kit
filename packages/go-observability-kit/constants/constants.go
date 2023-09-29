a25a0971c4338ab48a55dd86f16ffaf9d0997b54
Dilip Kola <kdilipkola@gmail.com>
2023-09-29 15:09:46 +0530

    feat: add go mod

--- packages/go-observability-kit/constants/constants.go
+++ packages/go-observability-kit/constants/constants.go
@@ -1,8 +1,9 @@
 package constants
 
 import (
-	"github.com/rudderlabs/go-observability-kit/common"
+	"github.com/rudderlabs/packages/go-observability-kit/common"
 )
+
 const (
 	TEST_NAME = common.Key("test_name")
 )
--- packages/go-observability-kit/go.mod
+++ packages/go-observability-kit/go.mod
@@ -1,3 +1,3 @@
-module github.com/rudderlabs/go-observability-kit
+module github.com/rudderlabs/packages/go-observability-kit
 
 go 1.21.1
