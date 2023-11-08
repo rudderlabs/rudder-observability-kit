498385dafbe364017f57774acd8a045fbd1b093f
Francesco Casula <fra.casula@gmail.com>
2023-11-08 17:28:15 +0100

    chore: dropping generics

--- go/labels/label.go
+++ go/labels/label.go
@@ -2,68 +2,108 @@ package labels
 
 import "go.uber.org/zap"
 
-type Label[T any] struct {
-	name  string
-	value T
+type FieldType uint8
+
+const (
+	UnknownType FieldType = iota
+	StringType
+	IntType
+	BoolType
+	FloatType
+)
+
+type Label struct {
+	name      string
+	fieldType FieldType
+
+	unknown any
+	string  string
+	int     int64
+	bool    bool
+	float   float64
 }
 
-func NewLabel[T any](name string, value T) Label[T] {
-	return Label[T]{name: name, value: value}
-}
-
-func name[T any](name string) func(T) Label[T] {
-	return func(value T) Label[T] {
-		return NewLabel[T](name, value)
+func NewLabel(name string, value any) Label {
+	return Label{
+		name:    name,
+		unknown: value,
 	}
 }
 
-func (l Label[T]) Name() string {
+func StringLabel(name, value string) Label {
+	return Label{
+		name:      name,
+		string:    value,
+		fieldType: StringType,
+	}
+}
+
+func IntLabel(name string, value int64) Label {
+	return Label{
+		name:      name,
+		int:       value,
+		fieldType: IntType,
+	}
+}
+
+func BoolLabel(name string, value bool) Label {
+	return Label{
+		name:      name,
+		bool:      value,
+		fieldType: BoolType,
+	}
+}
+
+func FloatLabel(name string, value float64) Label {
+	return Label{
+		name:      name,
+		float:     value,
+		fieldType: FloatType,
+	}
+}
+
+func (l Label) Name() string {
 	return l.name
 }
 
-func (l Label[T]) Value() T {
-	return l.value
+func (l Label) Value() any {
+	switch l.fieldType {
+	case StringType:
+		return l.string
+	case IntType:
+		return l.int
+	case BoolType:
+		return l.bool
+	case FloatType:
+		return l.float
+	default:
+		return l.unknown
+	}
 }
 
 // Expand can be used with the go-kit sugared logger
 // e.g. logger.Infow("my message", labels.Expand(l1, l2, l3)...)
-func Expand(labels ...Label[any]) []any {
+func Expand(labels ...Label) []any {
 	result := make([]any, 0, len(labels)*2)
 	for _, label := range labels {
-		result = append(result, label.name, label.value)
+		result = append(result, label.name, label.Value())
 	}
 	return result
 }
 
 // Zap can be used with the go-kit non-sugared logger
 // e.g. logger.Infon("my message", l1.Zap(), l2.Zap(), l3.Zap())
-func (l Label[T]) Zap() zap.Field {
-	switch v := any(l.value).(type) {
-	case string:
-		return zap.String(l.name, v)
-	case bool:
-		return zap.Bool(l.name, v)
-	case int:
-		return zap.Int(l.name, v)
-	case float32:
-		return zap.Float32(l.name, v)
-	case float64:
-		return zap.Float64(l.name, v)
-	case error:
-		return zap.Error(v)
-	case int32:
-		return zap.Int32(l.name, v)
-	case int64:
-		return zap.Int64(l.name, v)
-	case uint:
-		return zap.Uint(l.name, v)
-	case uint32:
-		return zap.Uint32(l.name, v)
-	case uint64:
-		return zap.Uint64(l.name, v)
-	case []byte:
-		return zap.Binary(l.name, v)
+func (l Label) Zap() zap.Field {
+	switch l.fieldType {
+	case StringType:
+		return zap.String(l.name, l.string)
+	case IntType:
+		return zap.Int64(l.name, l.int)
+	case BoolType:
+		return zap.Bool(l.name, l.bool)
+	case FloatType:
+		return zap.Float64(l.name, l.float)
 	default:
-		return zap.Any(l.name, v)
+		return zap.Any(l.name, l.unknown)
 	}
 }
