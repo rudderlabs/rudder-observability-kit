57a26ecd298c37cc3174d6243f30721eb0cb1b73
Francesco Casula <fra.casula@gmail.com>
2023-11-15 16:47:41 +0100

    chore: using arg as test name

--- cmd/generate/main_test.go
+++ cmd/generate/main_test.go
@@ -1,49 +1,50 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestToCamelWithInitialisms(t *testing.T) {
 	tests := []struct {
-		test string
 		arg  string
 		want string
 	}{
-		{test: "1", arg: "destination_id", want: "DestinationID"},
-		{test: "2", arg: "someDestinationId", want: "SomeDestinationID"},
-		{test: "3", arg: "fooApi", want: "FooAPI"},
-		{test: "4", arg: "fooAscii", want: "FooASCII"},
-		{test: "5", arg: "fooCpu", want: "FooCPU"},
-		{test: "6", arg: "fooCss", want: "FooCSS"},
-		{test: "7", arg: "fooDns", want: "FooDNS"},
-		{test: "8", arg: "fooEof", want: "FooEOF"},
-		{test: "9", arg: "fooGuid", want: "FooGUID"},
-		{test: "10", arg: "fooHtml", want: "FooHTML"},
-		{test: "11", arg: "fooHttp", want: "FooHTTP"},
-		{test: "12", arg: "fooHttps", want: "FooHTTPS"},
-		{test: "13", arg: "fooId", want: "FooID"},
-		{test: "14", arg: "fooIp", want: "FooIP"},
-		{test: "15", arg: "fooJson", want: "FooJSON"},
-		{test: "16", arg: "fooQps", want: "FooQPS"},
-		{test: "17", arg: "fooRam", want: "FooRAM"},
-		{test: "18", arg: "fooRpc", want: "FooRPC"},
-		{test: "19", arg: "fooSsh", want: "FooSSH"},
-		{test: "20", arg: "fooTcp", want: "FooTCP"},
-		{test: "21", arg: "fooTls", want: "FooTLS"},
-		{test: "22", arg: "fooTtl", want: "FooTTL"},
-		{test: "23", arg: "fooUdp", want: "FooUDP"},
-		{test: "24", arg: "fooUi", want: "FooUI"},
-		{test: "25", arg: "fooUid", want: "FooUID"},
-		{test: "26", arg: "fooUuid", want: "FooUUID"},
-		{test: "27", arg: "fooUri", want: "FooURI"},
-		{test: "28", arg: "fooUrl", want: "FooURL"},
-		{test: "29", arg: "fooXml", want: "FooXML"},
-		{test: "30", arg: "fooXss", want: "FooXSS"},
+		{arg: "destination_id", want: "DestinationID"},
+		{arg: "someDestinationId", want: "SomeDestinationID"},
+		{arg: "fooApi", want: "FooAPI"},
+		{arg: "fooAscii", want: "FooASCII"},
+		{arg: "fooCpu", want: "FooCPU"},
+		{arg: "fooCss", want: "FooCSS"},
+		{arg: "fooDns", want: "FooDNS"},
+		{arg: "fooEof", want: "FooEOF"},
+		{arg: "fooGuid", want: "FooGUID"},
+		{arg: "fooHtml", want: "FooHTML"},
+		{arg: "fooHttp", want: "FooHTTP"},
+		{arg: "fooHttps", want: "FooHTTPS"},
+		{arg: "fooId", want: "FooID"},
+		{arg: "fooIp", want: "FooIP"},
+		{arg: "fooJson", want: "FooJSON"},
+		{arg: "fooQps", want: "FooQPS"},
+		{arg: "fooRam", want: "FooRAM"},
+		{arg: "fooRpc", want: "FooRPC"},
+		{arg: "fooSsh", want: "FooSSH"},
+		{arg: "fooTcp", want: "FooTCP"},
+		{arg: "fooTls", want: "FooTLS"},
+		{arg: "fooTtl", want: "FooTTL"},
+		{arg: "fooUdp", want: "FooUDP"},
+		{arg: "fooUi", want: "FooUI"},
+		{arg: "fooUid", want: "FooUID"},
+		{arg: "fooUuid", want: "FooUUID"},
+		{arg: "fooUri", want: "FooURI"},
+		{arg: "fooUrl", want: "FooURL"},
+		{arg: "fooXml", want: "FooXML"},
+		{arg: "fooXss", want: "FooXSS"},
 	}
 	for _, tt := range tests {
-		t.Run(tt.test, func(t *testing.T) {
-			if got := toCamelWithInitialisms(tt.arg); got != tt.want {
-				t.Errorf("toCamelWithInitialisms() = %v, want %v", got, tt.want)
-			}
+		t.Run(tt.arg, func(t *testing.T) {
+			require.Equal(t, tt.want, toCamelWithInitialisms(tt.arg))
 		})
 	}
 }
